5d40e38bf461ea1c9cd68922f863f97bd65a7119
Xing Qiu <xing_qiu@urmc.rochester.edu>
2019-11-01 17:49:21 -0400

    Added row name 'fixed' to the first row of multi_sparse_score to improve readability.

--- R/PredScore.r
+++ R/PredScore.r
@@ -258,7 +258,7 @@ score_func = function(mod1, Data_0, Data_1, Response_interaction_index, Response
     #====== step4: generate sparse multiple scores =========#
     #=======================================================#
 
-    multi_sparse_score = rbind(colSums(res * (delta_sparse_fix)),
+    multi_sparse_score = rbind("fixed"=colSums(res * (delta_sparse_fix)),
                                         (res * delta_sparse_random)[union_index,]) ### m by n
 
 
@@ -345,7 +345,7 @@ score_func = function(mod1, Data_0, Data_1, Response_interaction_index, Response
     #====== step4: generate sparse multiple scores =========#
     #=======================================================#
 
-    multi_sparse_score = c(colSums(res * (delta_sparse_fix)),
+    multi_sparse_score = c("fixed"=colSums(res * (delta_sparse_fix)),
                                (res * delta_sparse_random)[union_index,]) ### m by n
 
 
